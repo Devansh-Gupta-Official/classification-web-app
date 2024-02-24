7341e124fc6c0f31a951a382bf4f439f1f2200c2
Devansh-Gupta-Official <devanshcool.gupta@gmail.com>
2024-02-24 19:46:22 +0530

    function to train models

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 import streamlit as st
 from sklearn import datasets
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
+from sklearn.ensemble import RandomForestClassifier
 import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from sklearn.decomposition import PCA
+import matplotlib.pyplot as plt
 
 st.title("Explore Different Classifiers")
 st.write("### Which one is the best?")
@@ -44,9 +51,22 @@ def add_parameter(classifier_name):
         max_depth = st.sidebar.slider("Max Depth",2,15)
         n_estimators = st.sidebar.slider("Number of Estimators",1,100)
         criterion = st.sidebar.selectbox("Criterion",["gini", "entropy", "log_loss"])
+        random_state = st.sidebar.number_input("Random State",0,1000000)
         params["max_depth"] = max_depth
         params["n_estimators"] = n_estimators           
-        params["criterion"] = criterion           
+        params["criterion"] = criterion     
+        params["random"] = random_state      
     return params
     
-add_parameter(classifier_name)
+params = add_parameter(classifier_name)
+
+def get_classifier(classifier_name,params):
+    if classifier_name=="KNN":
+        classifier = KNeighborsClassifier(n_neighbors=params["K"])
+    elif classifier_name=="SVM":
+        classifier = SVC(C=params["C"],degree=params["degree"],kernel=params["kernel"])
+    else:
+        classifier=RandomForestClassifier(max_depth=params["max_depth"],n_estimators=params["n_estimators"],criterion=params["criterion"],random_state=params["random"])
+    return classifier
+
+classifier = get_classifier(classifier_name,params)
