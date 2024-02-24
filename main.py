14fd13a0b71ce9331eb2283f1fc0c5ef69ad805e
Devansh-Gupta-Official <devanshcool.gupta@gmail.com>
2024-02-24 19:27:50 +0530

    function to input parameters for each classifier

--- main.py
+++ main.py
@@ -25,4 +25,28 @@ X, y = get_dataset(df_name)
 st.write("Shape of Dataset", X.shape)
 st.write("Number of Classes", len(np.unique(y)))
 
-
+def add_parameter(classifier_name):
+    params=dict()
+    if classifier_name=="KNN":
+        #number of neighbours
+        K = st.sidebar.slider("Number of neighbors",1,15)        
+        params["K"] = K
+    elif classifier_name=="SVM":
+        #Regularization parameter
+        C =  st.sidebar.slider("Regularization parameter",0.01,10.0)   
+        degree = st.sidebar.slider("Degree",1,10)
+        kernel = st.sidebar.selectbox("Kernel",["linear", "poly", "rbf", "sigmoid"])
+        params["C"] = C
+        params["degree"] = degree
+        params["kernel"] = kernel
+    else:
+        #max depth
+        max_depth = st.sidebar.slider("Max Depth",2,15)
+        n_estimators = st.sidebar.slider("Number of Estimators",1,100)
+        criterion = st.sidebar.selectbox("Criterion",["gini", "entropy", "log_loss"])
+        params["max_depth"] = max_depth
+        params["n_estimators"] = n_estimators           
+        params["criterion"] = criterion           
+    return params
+    
+add_parameter(classifier_name)
